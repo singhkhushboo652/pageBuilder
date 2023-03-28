2a4348a042411bb0def716121d10890bd56309a1
Arnab Biswas <arnab@eigerlabtech.com>
2023-03-28 10:47:25 +0530

    added auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PageComponentsModule } from './page-components/page-components.module';
 import { WarRoomModule } from './war-room/war-room.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/madfunnel'), PageComponentsModule, WarRoomModule],
+  // imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/madfunnel'), PageComponentsModule, WarRoomModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://172.17.0.4:27017/madfunnel'),
+    PageComponentsModule,
+    WarRoomModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
