3c00145b7dddc41e77a72cd6c90c1b290f221310
singhkhushboo652 <singh.khushboo652@gmail.com>
2023-03-29 14:31:06 +0530

    change host of mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PageComponentsModule } from './page-components/page-components.module';
 import { WarRoomModule } from './war-room/war-room.module';
 import { AuthModule } from './auth/auth.module';
-
+ 
 @Module({
   // imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/madfunnel'), PageComponentsModule, WarRoomModule],
   imports: [
-    MongooseModule.forRoot('mongodb://172.17.0.4:27017/madfunnel'),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/madfunnel'),
     PageComponentsModule,
     WarRoomModule,
     AuthModule,
